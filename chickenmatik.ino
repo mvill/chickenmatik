9a3ea2a76bf913c00c9bc0c1a75167c80aca922f
mvill <mathias.villeger@gmail.com>
2017-07-23 19:52:00 +0200

    some tests...

--- chickenmatik.ino
+++ chickenmatik.ino
@@ -1,47 +1,98 @@
-/*
-  LiquidCrystal Library - Hello World
- 
- Demonstrates the use a 16x2 LCD display.  The LiquidCrystal
- library works with all LCD displays that are compatible with the 
- Hitachi HD44780 driver. There are many of them out there, and you
- can usually tell them by the 16-pin interface.
- 
- This sketch prints "Hello World!" to the LCD
- and shows the time.
- 
-  The circuit:
- * LCD RS pin to digital pin 12
- * LCD Enable pin to digital pin 11
- * LCD D4 pin to digital pin 5
- * LCD D5 pin to digital pin 4
- * LCD D6 pin to digital pin 3
- * LCD D7 pin to digital pin 2
- * LCD R/W pin to ground
- * 10K resistor:
- * ends to +5V and ground
- * wiper to LCD VO pin (pin 3)
- 
- Library originally added 18 Apr 2008
- by David A. Mellis
- library modified 5 Jul 2009
- by Limor Fried (http://www.ladyada.net)
- example added 9 Jul 2009
- by Tom Igoe
- modified 22 Nov 2010
- by Tom Igoe
- 
- This example code is in the public domain.
-
- http://www.arduino.cc/en/Tutorial/LiquidCrystal
- */
-
-// include the library code:
 #include <LiquidCrystal.h>
+#include "menu.h"
+
+const int LEFT_BUTTON_PIN = 6;
+const int DOWN_BUTTON_PIN = 7;
+const int UP_BUTTON_PIN = 8;
+const int RIGHT_BUTTON_PIN = 9;
+const int OK_BUTTON_PIN = 10;
+
+const int NB_BUTTONS = 5;
+
+
+Button_t downButton = {
+      DOWN_BUTTON_PIN,
+      "DOWN",
+      LOW
+    };
+Button_t leftButton = {
+      LEFT_BUTTON_PIN,
+      "LEFT",
+      LOW
+    };
+Button_t upButton = {
+      UP_BUTTON_PIN,
+      "UP",
+      LOW
+    };
+Button_t rightButton = {
+      RIGHT_BUTTON_PIN,
+      "RIGHT",
+      LOW
+    };
+Button_t okButton = {
+      OK_BUTTON_PIN,
+      "OK",
+      LOW
+    };
+    
+Button_t buttons[] = {downButton, leftButton, upButton, rightButton, okButton};
+
+
+
 
 // initialize the library with the numbers of the interface pins
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 
+
+void handleButtonPressed(Button_t &button){
+  lcd.setCursor(0, 1);
+  //lcd.print(button.label);
+  lcd.print(String(button.label + " HIGH"));
+  if( button.state == HIGH){
+    lcd.print(String(button.label + " HIGH"));
+  }
+  else{
+    lcd.print(String(button.label + " LOW"));
+  }
+  
+}
+
+
+void refreshButtonsState(){
+  
+//      Serial.println(String(" si zeo f(buttons) : " + sizeof(buttons)));
+      //Serial.println(sizeof(buttons));
+  
+  for (int i = 0; i < NB_BUTTONS; i++) {
+    Button_t button = buttons[i];
+    int newState = digitalRead(button.pin);
+    if( newState !=  button.state){
+        
+      Serial.println("AAAA");
+      //Serial.println(button.label);
+      Serial.println(button.state);
+      //Serial.println(String(newState));
+        button.state = newState;
+      Serial.println(button.state);
+      Serial.println("BBBB");
+        handleButtonPressed(button);
+    }
+  }
+}
+
 void setup() {
+  
+  Serial.begin(9600);
+  
+  pinMode(LEFT_BUTTON_PIN, INPUT);
+  pinMode(DOWN_BUTTON_PIN, INPUT);
+  pinMode(UP_BUTTON_PIN, INPUT);
+  pinMode(RIGHT_BUTTON_PIN, INPUT);
+  pinMode(OK_BUTTON_PIN, INPUT);
+  
+  
+  
   // set up the LCD's number of columns and rows: 
   lcd.begin(16, 2);
   // Print a message to the LCD.
@@ -49,10 +100,38 @@ void setup() {
 }
 
 void loop() {
+  
+  
+  // read the state of the pushbutton value:
+  refreshButtonsState();
+  
+  
   // set the cursor to column 0, line 1
   // (note: line 1 is the second row, since counting begins with 0):
   lcd.setCursor(0, 1);
   // print the number of seconds since reset:
-  lcd.print(millis()/1000);
+  //lcd.print(millis()/1000);
+  
+  /*
+  if (leftButtonState == HIGH) {      
+    lcd.print("LEFT ");
+  } 
+  else if (downButtonState == HIGH) { 
+    lcd.print("DOWN ");
+  }
+  else if (upButtonState == HIGH) { 
+    lcd.print("UP   ");
+  }
+  else if (rightButtonState == HIGH) { 
+    lcd.print("RIGHT");
+  }
+  else if (okButtonState == HIGH) { 
+    displayMenu(MAIN_MENU);
+    lcd.print("OK   ");
+  }
+  else{
+    lcd.print("     ");
+  }*/
+  
 }
 
